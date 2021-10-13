ecdcc9049e34471a0dda743e915905a78dfa2047
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-10-13 22:03:57 +0200

    feat: turn the whole thing into a mono repo

---
+++ packages/rudi/commands/init.ts
@@ -0,0 +1,24 @@
+import prompts, { PromptObject } from 'prompts'
+
+export async function init() {
+    let questions: PromptObject[] = [
+        {
+            type: 'text',
+            name: 'name',
+            message: 'Project name?',
+        },
+        {
+            type: 'select',
+            name: 'type',
+            message: 'select your project type?',
+            choices: [
+                { title: 'cli', value: 'cli' },
+                { title: 'basic typescript project', value: 'simple' },
+                { title: 'react-component', value: 'react' },
+            ],
+        },
+    ]
+
+    const response = await prompts(questions)
+    console.log({ response })
+}
