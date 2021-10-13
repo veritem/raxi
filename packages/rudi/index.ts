f30ef618b62da9e2c3ef560ec0fdfadf84bdf283
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-10-13 22:13:51 +0200

    feat: turn the whole thing into a mono repo

--- packages/rudi/index.ts
+++ packages/rudi/index.ts
@@ -1,35 +1,35 @@
 #!/usr/bin/env node
 
-import sade from 'sade';
+import sade from 'sade'
 
-import { init } from './commands/init';
+import { init } from './commands/init'
 
-const program = sade('rudi');
+const program = sade('rudi')
 
-program.version('0.0.1');
+program.version('0.0.1')
 
 program
-  .command('build')
-  .describe('Build the project')
-  .action((src, dest, opts) => {
-    console.log({ src });
-    console.log({ dest });
-    console.log({ opts });
-  })
-  .command('init')
-  .describe('Scafold new project')
-  .action(async () => {
-    await init();
-  })
-  .command('lint')
-  .describe('lint your project')
-  .action(() => {
-    console.log('lint your project');
-  })
-  .command('test')
-  .describe('Run tests for your project')
-  .action(() => {
-    console.log('testing your application');
-  });
+    .command('build')
+    .describe('Build the project')
+    .action((src, dest, opts) => {
+        console.log({ src })
+        console.log({ dest })
+        console.log({ opts })
+    })
+    .command('init')
+    .describe('Scafold new project')
+    .action(async () => {
+        await init()
+    })
+    .command('lint')
+    .describe('lint your project')
+    .action(() => {
+        console.log('lint your project')
+    })
+    .command('test')
+    .describe('Run tests for your project')
+    .action(() => {
+        console.log('testing your application')
+    })
 
-program.parse(process.argv);
+program.parse(process.argv)
